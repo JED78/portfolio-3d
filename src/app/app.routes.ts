6886d79123f93221672810655af6f6d78d626592
Jose Esteban de la Cruz Chico <delacruzchico@hotmail.es>
2026-02-12 15:01:24 +0100

    Version con imagen.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Scene3dComponent } from './components/scene3d/scene3d';
+import { Header } from '../app/pages/header/header';
 export const routes: Routes = [
   {
     path: '',
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Scene3dComponent } from './components/scene3d/scene3d';
-
+import { Header } from '../app/pages/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Scene3dComponent],
+  imports: [Header, Scene3dComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
